40f444564d0503536fe92aae5be0aa841165d829
deepakgoswami <deepakgoswami@bricknbolt.com>
2023-07-28 00:17:00 +0530

    added components and home screen change

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from './pages/home'
+import About from './pages/about'
+import Resume from './pages/resume'
+import Contact from './pages/contact'
+import Projects from './pages/projects'
+import NoPage from './pages/noPage'
+import NavBar from './components/navBar'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <NavBar />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />}>
+            <Route index element={<Home />} />
+            <Route path="about" element={<About />} />
+            <Route path="resume" element={<Resume />} />
+            <Route path="projects" element={<Projects />} />
+            <Route path="contact" element={<Contact />} />
+            <Route path="*" element={<NoPage />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
---
+++ src/constant/data.js
@@ -0,0 +1,24 @@
+export const NAME = "Deepak Goswami"
+export const DESCRIPTION = "I'm a passionate Software Developer from Bengaluru"
+export const TABS = [
+    {
+        title:"Home",
+        id:"home"
+    },
+    {
+        title:"About",
+        id:"about"
+    },
+    {
+        title:"Resume",
+        id:"resume"
+    },
+    {
+        title:"Projects",
+        id:"projects"
+    },
+    {
+        title:"Contact",
+        id:"contact"
+    }
+]
